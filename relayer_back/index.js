72255c0a76ff1f3ebe82e773510b5dfb008961b5
Naveen pandian <npandian515@gmail.com>
2025-10-17 15:10:01 +0530

    bro installed redis for cache and majorly imported on index

--- relayer_back/index.js
+++ relayer_back/index.js
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const express = require('express');
+const cors = require('cors');
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit")
+const {ethers} = require("ethers")
+const Redis = require("redis")
+const Queue = require("bull");
+require('dotenv').config();
