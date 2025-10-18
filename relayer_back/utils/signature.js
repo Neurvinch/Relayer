dd0b32cd747c613d5b6e882e5cce23d2f814dbb5
Naveen pandian <npandian515@gmail.com>
2025-10-18 20:28:43 +0530

    the forward req types are encoded

--- relayer_back/utils/signature.js
+++ relayer_back/utils/signature.js
@@ -15,6 +15,19 @@ class SignatureUtils {
                 verifingContract: forwarderAddress,
             };
 
+            const types = {
+                ForwardRequest : [
+                    {name: 'from', type: 'address'},
+                      { name: 'to', type: 'address' },
+                    { name: 'value', type: 'uint256' },
+                    {name: 'gas', type:'uint256' },
+                    {name: 'nonce', type: 'uint256' },
+                    {name: 'deadline', type: 'uint256' },
+                    {name: 'data', type: 'bytes' },
+
+                ]
+            };
+
 
             
         } catch (error) {
