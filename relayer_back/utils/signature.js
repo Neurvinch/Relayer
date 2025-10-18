694b019e65f6fbcaf787582436bb81a29ab20c34
Naveen pandian <npandian515@gmail.com>
2025-10-18 20:37:25 +0530

    the creating of the signature static is on

--- relayer_back/utils/signature.js
+++ relayer_back/utils/signature.js
@@ -42,4 +42,31 @@ class SignatureUtils {
                 return false;
         }
     }
+
+    static async createSignature(request, privatekey, forwarderAddress, chainId){
+
+        const wallet = new ethers.Wallet(privatekey)
+
+        const domain = {
+            name: 'RelayerDAppForwarder',
+            version: '1',
+            chainId: chainId,
+            verifyingContract: forwarderAddress
+        };
+
+
+        const types= {
+              ForwardRequest: [
+                { name: 'from', type: 'address' },
+                { name: 'to', type: 'address' },
+                { name: 'value', type: 'uint256' },
+                { name: 'gas', type: 'uint256' },
+                { name: 'nonce', type: 'uint256' },
+                { name: 'deadline', type: 'uint48' },
+                { name: 'data', type: 'bytes' }
+                ]
+        };
+
+        return await 
+    }
 }
