2bb54532c93a8179a859e0de48ec7049442238c0
neuervinch <npandian515@gmail.com>
2025-10-19 19:49:18 +0530

    the network is  configured a filed

---
+++ relayer_back/config/network.js
@@ -0,0 +1,9 @@
+module.exorts = {
+  localhost: {
+    name: "localhost",
+    chainId: 31337,
+    rpcUrl: "http://127.0.0.1:8545",
+    forwarderAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    blockTime: 2000,
+  },
+};
